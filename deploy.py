7525cb537111019c3b878a6ff25cd5896c253a4e
KerenElience <1178841595@qq.com>
2026-06-23 11:17:35 +0800

    modified:   deploy.py

--- deploy.py
+++ deploy.py
@@ -1,2 +1,52 @@
 import torch
 import torch.nn as nn
+import onnx
+from src.unet import UNet
+from src.resunet import ResUnet
+from src.attention import AttUnet
+from src.resunet_tuned import AttUnetPretrained
+from src.data import MAX_HEIGHT, MAX_WIDTH
+from utils.argparse import deploy_args
+
+def get_model(backbone: nn.Module, weight: str):
+    model_state = torch.load(weight)
+    model = backbone.load_state_dict(model_state["state_dict"])
+    model = torch.jit.script(model)
+    return model
+
+def check_model(model_path):
+    onnx_model = onnx.load(model_path)
+    onnx.checker.check_model(onnx_model, full_check=True)
+
+def export_onnx(model, savepath):
+    model.eval()
+    dummp_input = torch.randn([32, 3, MAX_HEIGHT, MAX_WIDTH])
+    try:
+        torch.onnx.export(model, dummp_input, f = savepath,
+                          dynamic_axes={"input": {0: "batch_size"},
+                                        "output": {0: "batch_size"}})
+        check_model(savepath)
+        return True
+    except:
+        return False
+    
+def main(args):
+    if args.model == 'unet':
+        model = UNet(3, 21)
+    elif args.model == "resunet":
+        model = ResUnet(3, 21)
+    elif args.model == "attunet":
+        model = AttUnet(3, 21)
+    elif args.model == "resunet-tuned":
+        model = AttUnetPretrained(3, 21)
+    
+    model = get_model(model, args.weight)
+    isexported = export_onnx(model, args.output)
+    if isexported:
+        print("Model output into onnx successfully.")
+    else:
+        raise ValueError("Model outputo into onnx failed.")
+
+if __name__ == "__main__":
+    args = deploy_args()
+    main(args)
--- utils/argparse.py
+++ utils/argparse.py
@@ -13,3 +13,16 @@ def train_args():
     parser.add_argument("--resume", default="", type = str, metavar="PATH", help="path to checkpoint (default: None)")
     parser.add_argument("--name", default="default", type = str, help = "experiment name")
     return parser.parse_args()
+
+def deploy_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, 
+                        help = "choose a model [unet, resunet, attunet, resunet-tuned]", default="attunet")
+    parser.add_argument("--batch_size", default=16, type = int, help="number of total epochs to run")
+    parser.add_argument("--amp", default=True, type=bool, help="whether to enable mixed precision training mode")
+    parser.add_argument("--device", default="auto", type = str, help="which device will be used")
+    parser.add_argument("--weight", default="", type = str, metavar="PATH", help="path to checkpoint (default: None)")
+    parser.add_argument("--name", default="default", type = str, help = "experiment name")
+
+    parser.add_argument("-o", dest="--output", default="./unet.onnx", type=str, metavar="PATH", help="onnx savepath")
+    return parser.parse_args()
